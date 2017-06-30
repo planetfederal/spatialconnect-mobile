e595c50d6e0b2fbd55acb658245a78f82214c387
Landon Robinson <lrobinson@boundlessgeo.com>
2017-06-30 15:55:27 -0400

    Add push notification to iOS (FCM) (#122)
    * Adding FCM to ios
    
    * Removing extra space
    
    * Adding back dsiable for OS_ACTIVITY_MODE

--- ios/SCMobile/AppDelegate.m
+++ ios/SCMobile/AppDelegate.m
@@ -11,13 +11,31 @@
 #import <React/RCTRootView.h>
 #import <React/RCTBundleURLProvider.h>
 #import <Crashlytics/Crashlytics.h>
+#import "Firebase.h"
+
+#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+@import UserNotifications;
+#endif
+
+// Implement UNUserNotificationCenterDelegate to receive display notification via APNS for devices
+// running iOS 10 and above.
+#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+@interface AppDelegate () <UNUserNotificationCenterDelegate>
+@end
+#endif
 
 @implementation AppDelegate
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
   [Crashlytics startWithAPIKey:@"93c312957d2dbf3351bffe24bea587c15ab7a893"];
-
+  
+  //initialize Firebase messaging
+  [FIRApp configure];
+  
+  //set messaging delegate
+  [FIRMessaging messaging].delegate = self;
+  
   NSURL *jsCodeLocation;
 
  //Loading JavaScript code
@@ -39,10 +57,84 @@
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
-
-
+  [self registerForNotifications];
+  
   return YES;
 }
 
+- (void)messaging:(nonnull FIRMessaging *)messaging didRefreshRegistrationToken:(nonnull NSString *)fcmToken {
+  // Note that this callback will be fired everytime a new token is generated, including the first
+  // time. So if you need to retrieve the token as soon as it is available this is where that
+  // should be done.
+  NSLog(@"FCM registration token: %@", fcmToken);
+  
+  // TODO: send to SDK.
+}
+
+- (void)application:(UIApplication *)application didFailToRegisterForRemoteNotificationsWithError:(NSError *)error {
+  NSLog(@"Unable to register for remote notifications: %@", error);
+}
+
+
+- (void)application:(UIApplication *)application didReceiveRemoteNotification:(NSDictionary *)userInfo {
+  // If you are receiving a notification message while your app is in the background,
+  // this callback will not be fired till the user taps on the notification launching the application.
+  NSLog(@"Got remote notification %@", userInfo);
+}
+
+
+/* ****************************************************************/
+/* Receive displayed notifications messages for iOS 10 devices.   */
+/* ****************************************************************/
+
+#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+// Handle incoming notification messages while app is in the foreground.
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+       willPresentNotification:(UNNotification *)notification
+         withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler {
+  NSDictionary *userInfo = notification.request.content.userInfo;
+  
+  NSLog(@"Handling incoming notification while in foreground %@", userInfo);
+  
+  // Change this to your preferred presentation option
+  completionHandler(UNNotificationPresentationOptionNone);
+}
+
+// Handle notification messages after display notification is tapped by the user.
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center
+didReceiveNotificationResponse:(UNNotificationResponse *)response
+         withCompletionHandler:(void (^)())completionHandler {
+  NSDictionary *userInfo = response.notification.request.content.userInfo;
+  
+  NSLog(@"user tapped display notification %@", userInfo);
+  
+  completionHandler();
+}
+#endif
+
+- (void)registerForNotifications {
+
+  if (floor(NSFoundationVersionNumber) <= NSFoundationVersionNumber_iOS_9_x_Max) {
+    UIUserNotificationType allNotificationTypes =
+    (UIUserNotificationTypeSound | UIUserNotificationTypeAlert | UIUserNotificationTypeBadge);
+    UIUserNotificationSettings *settings =
+    [UIUserNotificationSettings settingsForTypes:allNotificationTypes categories:nil];
+    [[UIApplication sharedApplication] registerUserNotificationSettings:settings];
+  } else {
+    // iOS 10 or later
+#if defined(__IPHONE_10_0) && __IPHONE_OS_VERSION_MAX_ALLOWED >= __IPHONE_10_0
+    // For iOS 10 display notification (sent via APNS)
+    [UNUserNotificationCenter currentNotificationCenter].delegate = self;
+    UNAuthorizationOptions authOptions =
+    UNAuthorizationOptionAlert
+    | UNAuthorizationOptionSound
+    | UNAuthorizationOptionBadge;
+    [[UNUserNotificationCenter currentNotificationCenter] requestAuthorizationWithOptions:authOptions completionHandler:^(BOOL granted, NSError * _Nullable error) {
+    }];
+#endif
+  }
+  
+  [[UIApplication sharedApplication] registerForRemoteNotifications];
+}
 
 @end
