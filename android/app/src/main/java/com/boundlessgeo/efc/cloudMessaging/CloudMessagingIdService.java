fb5eea4d3f6a539c18a15f73eace8ccec7987064
georific <lrobinson@boundlessgeo.com>
2017-07-18 23:43:10 -0400

    Update device token for push to sdk

--- android/app/src/main/java/com/boundlessgeo/efc/cloudMessaging/CloudMessagingIdService.java
+++ android/app/src/main/java/com/boundlessgeo/efc/cloudMessaging/CloudMessagingIdService.java
@@ -15,7 +15,7 @@
 package com.boundlessgeo.efc.cloudMessaging;
 
 import android.util.Log;
-
+import com.boundlessgeo.spatialconnect.SpatialConnect;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
@@ -33,6 +33,6 @@ public class CloudMessagingIdService extends FirebaseInstanceIdService {
 
     private void sendRegistrationTokenToServer(String token) {
         Log.d(LOG_TAG, "cloud token: " + token);
-        // TODO: Implement this method to send token mqtt.
+        SpatialConnect.getInstance().updateDeviceToken(token);
     }
 }
